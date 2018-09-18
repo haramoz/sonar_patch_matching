0b4353bed42cebc86c9c30a821feeb3282a10cc5
haramoz <arkamallick30@gmail.com>
2018-09-18 22:13:22 +0200

    adding builder_densenet

---
+++ builder_densenet.py
@@ -0,0 +1,108 @@
+# Code extracted from keras_applications repo
+
+from keras import layers, models, backend
+
+
+def dense_block(x, blocks, name):
+    """A dense block.
+    # Arguments
+        x: input tensor.
+        blocks: integer, the number of building blocks.
+        name: string, block label.
+    # Returns
+        output tensor for the block.
+    """
+    for i in range(blocks):
+        x = conv_block(x, 32, name=name + '_block' + str(i + 1))
+    return x
+
+
+def transition_block(x, reduction, name):
+    """A transition block.
+    # Arguments
+        x: input tensor.
+        reduction: float, compression rate at transition layers.
+        name: string, block label.
+    # Returns
+        output tensor for the block.
+    """
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                  name=name + '_bn')(x)
+    x = layers.Activation('relu', name=name + '_relu')(x)
+    x = layers.Conv2D(int(backend.int_shape(x)[bn_axis] * reduction), 1,
+                      use_bias=False,
+                      name=name + '_conv')(x)
+    x = layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
+    return x
+
+
+def conv_block(x, growth_rate, name):
+    """A building block for a dense block.
+    # Arguments
+        x: input tensor.
+        growth_rate: float, growth rate at dense layers.
+        name: string, block label.
+    # Returns
+        Output tensor for the block.
+    """
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    x1 = layers.BatchNormalization(axis=bn_axis,
+                                   epsilon=1.001e-5,
+                                   name=name + '_0_bn')(x)
+    x1 = layers.Activation('relu', name=name + '_0_relu')(x1)
+    x1 = layers.Conv2D(4 * growth_rate, 1,
+                       use_bias=False,
+                       name=name + '_1_conv')(x1)
+    x1 = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                   name=name + '_1_bn')(x1)
+    x1 = layers.Activation('relu', name=name + '_1_relu')(x1)
+    x1 = layers.Conv2D(growth_rate, 3,
+                       padding='same',
+                       use_bias=False,
+                       name=name + '_2_conv')(x1)
+    x = layers.Concatenate(axis=bn_axis, name=name + '_concat')([x, x1])
+    return x
+
+# Blocks should be a list with growth rate for each dense block.
+# The total number of dense and transition blocks is defined by the
+# length of the blocks list.
+
+
+def build_densenet(blocks, include_top=True, input_shape=None,
+                   pooling=None, num_classes=1000):
+
+    img_input = layers.Input(shape=input_shape)
+
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
+    x = layers.Conv2D(64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='conv1/bn')(x)
+    x = layers.Activation('relu', name='conv1/relu')(x)
+    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
+    x = layers.MaxPooling2D(3, strides=2, name='pool1')(x)
+
+    for i, block in blocks:
+        conv_name = 'conv{}'.format(i+1)
+        pool_name = 'pool{}'.format(i+1)
+        x = dense_block(x, block, name=conv_name)
+        x = transition_block(x, 0.5, name=pool_name)
+
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
+    x = layers.Activation('relu', name='relu')(x)
+
+    if include_top:
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        x = layers.Dense(num_classes, activation='softmax', name='fc')(x)
+    else:
+        if pooling == 'avg':
+            x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        elif pooling == 'max':
+            x = layers.GlobalMaxPooling2D(name='max_pool')(x)
+
+    model = models.Model(img_input, x, name='densenet')
+
+    return model
