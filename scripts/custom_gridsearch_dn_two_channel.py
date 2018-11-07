4591de11f77b85dc4162b98b4a85901da11a0029
haramoz <arkamallick30.com>
2018-11-07 19:10:47 +0100

    pushing the final results of the densenet siamese

--- scripts/custom_gridsearch_dn_two_channel.py
+++ scripts/custom_gridsearch_dn_two_channel.py
@@ -38,6 +38,7 @@ from keras.models import load_model
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
 
 
 import pickle
@@ -45,118 +46,57 @@ seed = 7
 np.random.seed(seed)
 
 def process_data():
-    filename = '/home/amalli2s/thesis/keras/sonar_data'
-    infile = open(filename,'rb')
-    sonar_data_dict = pickle.load(infile)
-
-    infile.close()
-    x_train1 = sonar_data_dict.get("x_train1")
-    x_train2 = sonar_data_dict.get("x_train2")
-    y_train = sonar_data_dict.get("y_train")
-
-    x_val1 = sonar_data_dict.get("x_val1")
-    x_val2 = sonar_data_dict.get("x_val2")
-    y_val = sonar_data_dict.get("y_val")
-
-    x_test1 = sonar_data_dict.get("x_test1")
-    x_test2 = sonar_data_dict.get("x_test2")
-    y_test = sonar_data_dict.get("y_test")
-
-    #y_test_inverted = 1 - y_test
-    #y_train_inverted = 1 - y_train
-    #y_val_inverted = 1 - y_val
-    """x_train1 = np.moveaxis(x_train1, 1, -1)
-    x_train2 = np.moveaxis(x_train2, 1, -1)
-
-    x_val1 = np.moveaxis(x_val1, 1, -1)
-    x_val2 = np.moveaxis(x_val2, 1, -1)
-
-    x_test1 = np.moveaxis(x_test1, 1, -1)
-    x_test2 = np.moveaxis(x_test2, 1, -1)"""
+    f = h5py.File('/home/amalli2s/thesis/keras/matchedImagesSplitClasses-2017-02-24-17-39-44-96-96-split-val0.15-tr0.7-tst0.15.hdf5','r')
+    X_train = f['X_train'].value
+    y_train = f['y_train'].value
+    X_test = f['X_val'].value
+    y_test = f['y_val'].value
+    X_train_, X_val, y_train_, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=seed)
+    
+    return X_train,y_train,X_val,y_val,X_test,y_test
 
 
-    return x_train1, x_train2, y_train, x_val1, x_val2, y_val, x_test1, x_test2, y_test
-
-
-def create_base_network(layers,growth_rate,nb_dense_block,nb_filter,dropout_rate,reduction_,bn):
-    '''Base network to be shared (eq. to feature extraction).
-    '''    
-    nb_classes = 2
-    img_rows, img_cols = 96, 96
-    img_channels = 1
-
-    # Parameters for the DenseNet model builder
-    img_dim = (img_channels, img_rows, img_cols) if K.image_data_format() == 'channels_first' else (img_rows, img_cols, img_channels)
-
-    #depth = 22
-    #nb_dense_block = 4
-    #growth_rate = 30
-    #nb_filter = 32
-    #dropout_rate = 0.2  # 0.0 for data augmentation
-    classes = 2 #1
-    #reduction_ = 0.5
-    #bn = True
+def fit_model(data,layers,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,opt,reduction,bn,batch_size,pooling):
+    es_patience = 4    
+    lr_patience = 3    
+    weight_file = 'keras_densenet_twochannel_6Nov_1640_weights.h5' 
+    file_name = 'keras_densenet_twochannel_6Nov_1640'
     print("------------------------ current config for the test -------------------------")
-    print("Layers: ",layers," Growth_rate: ",growth_rate," nb_filter: ",nb_filter," dropout: ",dropout_rate)
-    print("dense_block ",nb_dense_block," reduction_: ",reduction_," bottleneck: ",bn)
+    print("Layers: ",layers," Growth_rate: ",growth_rate," nb_filter: ",nb_filter," dropout: ",dropout)
+    print("dense_block ",nb_dense_block," reduction: ",reduction," bottleneck: ",bn)
+    print("Epochs ",epochs," batch_size: ",batch_size," lr: ",lr," optimizer: ",opt)
+    print(" es_patience: ",es_patience," lr_patience: ",lr_patience)
     print("------------------------	  end of configs        -------------------------")
+
+    weight_file = 'keras_densenet_simple_wt_6Nov1610.h5'
+    img_dim = (2,96,96)
+    classes = 1 #for binary classification
 
     """input_shape=None, depth=40, nb_dense_block=3,growth_rate=12, nb_filter=-1,nb_layers_per_block=-1,
              bottleneck=False, reduction=0.0, dropout_rate=0.0, weight_decay=1e-4,subsample_initial_block=False,
              include_top=True,weights=None,input_tensor=None,pooling=None,classes=10,
              activation='softmax',transition_pooling='avg'"""
 
-    net =keras_densenet.DenseNet(nb_dense_block=nb_dense_block,
-                     growth_rate=growth_rate, nb_filter=nb_filter,
+    model=keras_densenet.DenseNet(nb_dense_block=nb_dense_block,
+                     growth_rate=growth_rate,
+                     nb_filter=nb_filter,
                      nb_layers_per_block=layers,
-                     dropout_rate=dropout_rate,
+                     dropout_rate=dropout,
                      input_shape=img_dim,
-                     include_top=False,
+                     include_top=True,
                      classes=classes,
-                     pooling='flatten',
+                     pooling=pooling,
                      bottleneck=bn,
-                     reduction=reduction_,
-                     #activation='sigmoid'
+                     reduction=reduction,
+                     activation='sigmoid',
                      weights=None,
                      subsample_initial_block=True,
-                     transition_pooling='max')
+                     transition_pooling='avg')    
 
-
-    
-    return net
-
-def fit_model(data,depth,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,opt,reduction,bn,batch_size,fc_dropout,fc_filter,fc_layers):
-    input_shape = (1,96,96)
-    es_patience = 4    
-    lr_patience = 3    
-    #batch_size = 64
-    weight_file = 'keras_densenet_siamese_4Nov_1540_weights.h5' 
-    file_name = 'keras_densenet_siamese_4Nov_1540' 
-    dense_dropout = 0.5
-    print("Epochs ",epochs," batch_size: ",batch_size," lr: ",lr," optimizer: ",opt)
-    print(" es_patience: ",es_patience," lr_patience: ",lr_patience)
-    print(" batch_size: ",batch_size," fc_dropout: ",fc_dropout," fc_filter: ",fc_filter," fc_layers: ",fc_layers)
-
-    base_network = create_base_network(depth,growth_rate,nb_dense_block,nb_filter,dropout,reduction,bn)
-    input_a = Input(shape=input_shape)
-    input_b = Input(shape=input_shape)
-
-    processed_a = base_network(input_a)
-    processed_b = base_network(input_b)
-
-    combined_features = concatenate([processed_a, processed_b], name = 'merge_features')
-
-    combined_features = Dense(fc_filter, kernel_initializer=keras.initializers.he_normal())(combined_features)    
-    combined_features = Activation('relu')(combined_features)
-    combined_features = BatchNormalization()(combined_features)
-    combined_features = Dropout(fc_dropout)(combined_features)
-
-    combined_features = Dense(1, activation = 'sigmoid')(combined_features)
-
-    model = Model(inputs = [input_a, input_b], outputs = [combined_features], name = 'model')
     model.summary()
+
     if opt=='adam':
-        optimizer = Adam(lr=lr)  # Using Adam instead of SGD to speed up training
+        optimizer = Adam(lr=lr)
     elif opt=='nadam':
         optimizer=Nadam(lr=lr)
     elif opt=='adadelta':        
@@ -180,18 +120,18 @@ def fit_model(data,depth,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,
     lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1),
                                cooldown=0, patience=lr_patience, min_lr=0.5e-6,verbose=1)
 
-    model.fit([data[0], data[1]],data[2],
+    model.fit(data[0],data[1],
           batch_size=batch_size,
           epochs=epochs,
-          validation_data=([data[3], data[4]],data[5]),
+          validation_data=(data[2], data[3]),
           callbacks=[es,lr_reducer],
           verbose=2)
     #model = load_model(weight_file) #This is the best model
 
-    score, acc = model.evaluate([data[6], data[7]],data[8], verbose=0)
+    score, acc = model.evaluate(data[4], data[5], verbose=0)
     print("Test accuracy:%0.3f"% acc)
-    pred = model.predict([data[6], data[7]])
-    auc_score = roc_auc_score(data[8],pred)
+    pred = model.predict(data[4])
+    auc_score = roc_auc_score(data[5],pred)
     auc_score = np.round(auc_score,4)
     print("current auc_score ------------------> %0.3f"%auc_score)
     if(auc_score > .94):
@@ -208,7 +148,7 @@ def fit_model(data,depth,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,
 
 def process_fit(config):
     data = process_data()
-    #nb_layers_per_block,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,opt,reduction,bn,fc_dropout,fc_filter,fc_layers
+    #nb_layers_per_block,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,opt,reduction,bn,pooling
     layers = []
     temp_layers = config[0].split("-")
     for i in temp_layers:
@@ -226,14 +166,12 @@ def process_fit(config):
     else:
         bn=True
     batch_size=int(config[10])
-    fc_dropout=float(config[11])
-    fc_filter=int(config[12])
-    fc_layers=int(config[13])
+    pooling=config[11]
 
     accs = []
     aucs = []
-    for i in range(10):
-        acc,auc = fit_model(data,layers,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,opt,reduction,bn,batch_size,fc_dropout,fc_filter,fc_layers)
+    for i in range(2):
+        acc,auc = fit_model(data,layers,growth_rate,nb_dense_block,nb_filter,dropout,lr,epochs,opt,reduction,bn,batch_size,pooling)
         accs.append(acc)
         aucs.append(auc)
     print("accuracies: ",accs)
@@ -251,7 +189,7 @@ if __name__ == '__main__':
     #accuracies = []
     auc_scores = []
     max_aucs = []
-    with open('./search_spaces/dn_siamese_random_27oct.csv') as csv_file:
+    with open('./search_spaces/dn_two_channel_random_6Nov.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
@@ -259,7 +197,7 @@ if __name__ == '__main__':
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             else:
-                #print(row)
+                print(row)
                 search_space.append(row)    
                 auc,max=process_fit(row)
                 auc_scores.append(auc)
